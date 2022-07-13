10d3261e744e28e69ca51817a88215dba52b0d7e
Chelly izraelov <hlly.lov@gmail.com>
2022-07-13 17:46:49 +0300

    linked list

---
+++ src/n03/linkedlist/n01/Magazine.java
@@ -0,0 +1,17 @@
+package n03.linkedlist.n01;
+
+public class Magazine {
+
+    // Represents a single magazine.
+    private String title;
+
+    // Sets up the new magazine with its title.
+    public Magazine(String newTitle) {
+        title = newTitle;
+    }
+
+    // Returns this magazine as a string.
+    public String toString() {
+        return title;
+    }
+}
---
+++ src/n03/linkedlist/n01/MagazineRack.java
@@ -0,0 +1,18 @@
+package n03.linkedlist.n01;
+
+public class MagazineRack {
+
+    // Creates a MagazineList object, adds several magazines to the list, then prints it.
+    public static void main(String[] args) {
+
+        MagazineList rack = new MagazineList();
+
+        rack.add(new Magazine("Time"));
+        rack.add(new Magazine("Woodworking Today"));
+        rack.add(new Magazine("Communications of the ACM"));
+        rack.add(new Magazine("House and Garden"));
+        rack.add(new Magazine("GQ"));
+
+        System.out.println(rack);
+    }
+}
